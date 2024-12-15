9c7aca472ff75b5b9d12bc8c7e1336bf2151c3d3
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-12-15 11:02:24 +0200

    added balance field to user model

--- user/models.py
+++ user/models.py
@@ -1,4 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    balance = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
