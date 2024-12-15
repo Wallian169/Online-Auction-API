2bb14d916cc651c4aa117dd73b15af1793d67851
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-12-15 12:06:22 +0200

    added field profile picture for User

--- user/models.py
+++ user/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
+    profile_pic = models.ImageField(upload_to="profile_pic", null=True, blank=True)
     email = models.EmailField(unique=True)
     balance = models.DecimalField(
         max_digits=10,
