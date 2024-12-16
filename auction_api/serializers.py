787b8289f7aec4028fdc88c7f2f65d1f0293f74c
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-12-16 11:06:40 +0200

    fixed auction lot creation logic
    How user is filled from request, which preventing user from creating lot on somebody's behalf

--- auction_api/serializers.py
+++ auction_api/serializers.py
@@ -2,13 +2,21 @@ from rest_framework import serializers
 from auction_api.models import AuctionLot, Bid
 
 class AuctionLotSerializer(serializers.ModelSerializer):
-    owner = serializers.StringRelatedField(read_only=True)
     bids = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     images = serializers.ImageField(use_url=True)
 
     class Meta:
         model = AuctionLot
-        fields = ["id", "item_name", "description", "created_at", "initial_price", "close_time", "images", "owner", "bids"]
+        fields = [
+            "id",
+            "item_name",
+            "description",
+            "created_at",
+            "initial_price",
+            "close_time",
+            "images",
+            "bids"
+        ]
 
 
 class BidSerializer(serializers.Serializer):
