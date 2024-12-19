f1180e5ba4fd9a2f082e21c30dfcc11c6e5e7cb3
Vadym Vyshnevskyi <87133740+Wallian169@users.noreply.github.com>
2024-12-19 12:33:47 +0200

    fixed reverse call of reset-password-confirm in user.signals.py

--- user/signals.py
+++ user/signals.py
@@ -24,8 +24,8 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         "username": reset_password_token.user.username,
         "email": reset_password_token.user.email,
         "reset_password_url": "{}?token={}".format(
-            instance.request.build_absolute_uri(reverse("password_reset:reset-password-confirm")),
-            reset_password_token.key)
+            instance.request.build_absolute_uri(reverse("user:password_reset:reset-password-confirm")),
+            reset_password_token.key) ## password_reset:reset-password-confirm
     }
 
     # render email text
