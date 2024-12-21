4076c22d67d88fa402a8ca420346cb4f921b4e83
Artem-user-19 <temakot254@gmail.com>
2024-12-21 20:58:40 +0200

    Validation for bids

--- auction_api/serializers.py
+++ auction_api/serializers.py
@@ -24,3 +24,22 @@ class BidSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bid
         fields = ["id", "auction_lot", "offered_price", "bidder", "bid_time"]
+
+    def validate_offered_price(self, value):
+        auction_lot = self.initial_data.get("auction_lot")
+        try:
+            auction_lot = AuctionLot.objects.get(pk=auction_lot)
+        except AuctionLot.DoesNotExist:
+            raise serializers.ValidationError("Auction Lot wasn`t found")
+
+        if value <= auction_lot.initial_price:
+            raise serializers.ValidationError(
+                "The bid must be higher then the initial price"
+            )
+        max_bid = auction_lot.bids.order_by("-offered_price").first()
+        if max_bid and value <= max_bid.offered_price:
+            raise serializers.ValidationError(
+                "The bid must be higher then the current highest bid"
+            )
+
+        return value
