95d677a35498ca8cef8aa05ef030611a9e27f47e
Vadym Vyshnevskyi <87133740+Wallian169@users.noreply.github.com>
2025-01-10 11:32:57 +0200

    created AuctionLotViewSet

--- auction_api/views.py
+++ auction_api/views.py
@@ -1,14 +1,28 @@
-from rest_framework import generics
+from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 
-from .models import AuctionLot, Bid
-from .serializers import AuctionLotSerializer, BidSerializer
+from auction_api.models import AuctionLot, Bid
+from auction_api.serializers import (
+    AuctionLotBaseSerializer,
+    AuctionLotSerializer,
+    BidSerializer,
+    AuctionLotDetailSerializer,
+)
 
 
-class AuctionLotListCreateView(generics.ListCreateAPIView):
+class AuctionLotViewSet(viewsets.ModelViewSet):
     queryset = AuctionLot.objects.all()
     serializer_class = AuctionLotSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return AuctionLotBaseSerializer
+        if self.action == "detail":
+            return AuctionLotDetailSerializer
+        if self.action == "place-bid":
+            return BidSerializer
+        return self.serializer_class
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -17,19 +31,15 @@ class AuctionLotListCreateView(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 
-class AuctionLotDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = AuctionLot.objects.all()
-    serializer_class = AuctionLotSerializer
-
 class BidListCreateView(generics.ListCreateAPIView):
     queryset = Bid.objects.all()
     serializer_class = BidSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        auction_lot = AuctionLot.objects.get(pk=self.kwargs['pk'])
+        auction_lot = AuctionLot.objects.get(pk=self.kwargs["pk"])
         return auction_lot.bids.all()
 
     def perform_create(self, serializer):
-        auction_lot = AuctionLot.objects.get(pk=self.kwargs['pk'])
+        auction_lot = AuctionLot.objects.get(pk=self.kwargs["pk"])
         serializer.save(bidder=self.request.user, auction_lot=auction_lot)
