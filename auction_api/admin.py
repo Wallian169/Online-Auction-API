4b547d10f591c787a09e16274ed8d1b405db3c46
Vadym Vyshnevskyi <87133740+Wallian169@users.noreply.github.com>
2025-01-17 13:42:06 +0200

    changed winner field default value to None

--- auction_api/admin.py
+++ auction_api/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 from auction_api.models import AuctionLot, Bid, AuctionLotImage, Category
 
-admin.site.register(AuctionLot)
+@admin.register(AuctionLot)
+class AuctionLotAdmin(admin.ModelAdmin):
+    exclude = ("winner",)
+
 admin.site.register(Bid)
 admin.site.register(AuctionLotImage)
 admin.site.register(Category)
