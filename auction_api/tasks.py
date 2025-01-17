5bc247fc88a72ffde6a0c10fb61a7c492e7787c5
Vadym Vyshnevskyi <87133740+Wallian169@users.noreply.github.com>
2025-01-17 12:39:45 +0200

    fixed task logic

--- auction_api/tasks.py
+++ auction_api/tasks.py
@@ -1,10 +1,12 @@
 from celery import shared_task
 from django.db.models import OuterRef, Subquery
 from django.utils.timezone import now
-from auction_api.models import AuctionLot, Bid
 
 @shared_task
 def close_auction_lots():
+    from auction_api.models import AuctionLot, Bid
+    print("Started closing auction lots...")
+
     max_bids = Bid.objects.filter(
         auction_lot=OuterRef("pk")
     ).order_by("-offered_price")
@@ -19,15 +21,20 @@ def close_auction_lots():
         found_winner=Subquery(max_bids.values("bidder_id")[:1])
     )
 
-    print(f"Found {expired_lots.count()} expired lots to close.")
+    if not expired_lots:
+        print("No expired auction lots found.")
+    else:
+        print(f"Found {expired_lots.count()} expired lots to close.")
 
     lots_to_close = []
     for expired_lot in expired_lots:
         print(f"Closing lot {expired_lot.id}: "
-              f"Winner is {expired_lot.found_winner}")
+              f"Winner id is {expired_lot.found_winner}")
         expired_lot.is_active = False
         expired_lot.winner_id = expired_lot.found_winner
         lots_to_close.append(expired_lot)
+
+    AuctionLot.objects.bulk_update(expired_lots, ["is_active", "winner_id"])
 
 
 @shared_task
