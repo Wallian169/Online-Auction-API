c4c2f68477f5e8a68504d856ac66422cd440c5fe
Vadym Vyshnevskyi <wallian169@gmail.com>
2025-02-19 17:01:48 +0200

    created toggle add/remove user favorites

--- auction_api/views.py
+++ auction_api/views.py
@@ -1,13 +1,13 @@
 from django.db.models import Count
-from django.db.models.functions import Random
-from drf_spectacular.utils import extend_schema, OpenApiParameter, extend_schema_view
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets, generics, status
 from rest_framework.decorators import action, api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from auction_api.models import AuctionLot, Bid, Category
+from auction_api.models import AuctionLot, Bid, Category, Favorite
 from auction_api.serializers import (
     AuctionLotBaseSerializer,
     AuctionLotSerializer,
@@ -22,6 +22,48 @@ class AuctionLotViewSet(viewsets.ModelViewSet):
     queryset = AuctionLot.objects.all()
     serializer_class = AuctionLotSerializer
     permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        summary="Toggle Favorite Lot",
+        description="Add or remove an auction lot from the user's favorites.",
+        responses={
+            200: {
+                "description": "Success message",
+                "content": {
+                    "application/json": {
+                        "example": {"detail": "Lot added to favorites."}
+                    }
+                },
+            },
+            404: {
+                "description": "Lot not found",
+                "content": {
+                    "application/json": {
+                        "example": {"detail": "Lot not found."}
+                    }
+                },
+            },
+        },
+        request=None,
+    )
+    @action(detail=True, methods=["POST"])
+    def toggle_favorite(self, request, pk=None):
+        """
+        Toggle a lot in the user's favorites.
+        Adds or removes the lot from the user's favorites.
+        """
+        user = request.user
+        lot = get_object_or_404(AuctionLot, pk=pk)
+        favorite, created = Favorite.objects.get_or_create(user=user, auction_lot=lot)
+
+        if created:
+            message = "Lot added to favorites."
+        else:
+            favorite.delete()
+            message = "Lot removed from favorites."
+
+        return Response({"detail": message}, status=status.HTTP_200_OK)
+
 
     def get_serializer_class(self):
         if self.action == "create":
