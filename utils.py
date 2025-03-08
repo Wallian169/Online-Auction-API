fc694f02497a053e1d3b1655abd7ee442e3b1c7a
Vadym Vyshnevskyi <wallian169@gmail.com>
2025-03-08 08:40:33 +0200

    fixed get_unique_image func

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
+import os
 import uuid
 
 
 def get_unique_image_name(
     filename: str,
 ) -> str:
-    name, ext = filename.split(".")
+    name, ext = os.path.splitext(filename)
     unique_filename = f"{name}-{uuid.uuid4().hex[:10]}.{ext}"
     return f"{unique_filename}"
