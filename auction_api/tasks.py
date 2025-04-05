8dcc935e8ccfdba4b7a17426992c947bac41efcf
Vadym Vyshnevskyi <wallian169@gmail.com>
2025-04-05 20:40:08 +0300

    added auto deletion of expired lots

--- auction_api/tasks.py
+++ auction_api/tasks.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from celery import shared_task
 from django.db.models import OuterRef, Subquery
 from django.utils.timezone import now
@@ -26,15 +28,26 @@ def close_auction_lots():
         print(f"Found {expired_lots.count()} expired lots to close.")
 
     lots_to_close = []
+    lots_to_delete = []
     for expired_lot in expired_lots:
         print(
             f"Closing lot {expired_lot.id}: " f"Winner id is {expired_lot.found_winner}"
         )
+        age = now() - expired_lot.close_time
         expired_lot.is_active = False
         expired_lot.winner_id = expired_lot.found_winner
+        if age > timedelta(days=3):
+            print(f"Deleting lot {expired_lot.id} — expired more than 3 days ago.")
+            lots_to_delete.append(expired_lot.id)
         lots_to_close.append(expired_lot)
 
-    AuctionLot.objects.bulk_update(expired_lots, ["is_active", "winner_id"])
+    if lots_to_close:
+        AuctionLot.objects.bulk_update(lots_to_close, ["is_active", "winner_id"])
+        print(f"Closed {len(lots_to_close)} lots.")
+
+    if lots_to_delete:
+        AuctionLot.objects.filter(id__in=lots_to_delete).delete()
+        print(f"Deleted {len(lots_to_delete)} lots.")
 
 
 @shared_task
